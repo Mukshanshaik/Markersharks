68a78e64c2f7c7b307b84c940bc16d92d4ca03f5
Muskhan <149375654+Mukshanshaik@users.noreply.github.com>
2024-08-23 21:15:36 +0530

    Update SupplierService.java

--- SupplierService.java
+++ SupplierService.java
@@ -1,5 +1,14 @@
 package markersharkassessementservice;
 
+@Service
 public class SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public Page<Supplier> querySuppliers(String location, String natureOfBusiness, String manufacturingProcesses, Pageable pageable) {
+        return supplierRepository.findByLocationAndNatureOfBusinessAndManufacturingProcesses(location, natureOfBusiness, manufacturingProcesses, pageable);
+  
+    }
 
 }
