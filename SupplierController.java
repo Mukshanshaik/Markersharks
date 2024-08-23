af9afd4feda2ab84765a90c26f1c9cad987659d3
Muskhan <149375654+Mukshanshaik@users.noreply.github.com>
2024-08-23 21:13:36 +0530

    Update SupplierController.java

--- SupplierController.java
+++ SupplierController.java
@@ -1,5 +1,20 @@
 package markersharkassessementcontroller;
 
+@RestController
+@RequestMapping("/api/supplier")
 public class SupplierController {
+    
+    @Autowired
+    private SupplierService supplierService;
+    
+    @PostMapping("/query")
+    public ResponseEntity<Page<Supplier>> querySuppliers(
+        @RequestBody SupplierQueryDTO queryDTO,
+        Pageable pageable) {
+        
+        Page<Supplier> suppliers = supplierService.querySuppliers(queryDTO, pageable);
+        return new ResponseEntity<>(suppliers, HttpStatus.OK);
+    }
+
 
 }
