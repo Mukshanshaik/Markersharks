1b6826ad8534ca1d1455ac8e025ae23c8e8f4550
Muskhan <149375654+Mukshanshaik@users.noreply.github.com>
2024-08-23 21:11:54 +0530

    Update GlobalExceptionHandler.java

--- GlobalExceptionHandler.java
+++ GlobalExceptionHandler.java
@@ -1,5 +1,23 @@
 package markersharkassessementGlobalExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
 
 }
